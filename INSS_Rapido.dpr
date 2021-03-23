7174ae85bdb973ecf56125a824d0290937673905
leandro.ma@ymail.com <lndrma>
2021-03-22 23:33:50 -0300

    teste le

---
+++ INSS_Rapido.dpr
@@ -0,0 +1,14 @@
+program INSS_Rapido;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  uLogin in 'uLogin.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
